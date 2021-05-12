12311601c2df9925d6d9f4d9a927cd8f7edfb0d5
nhyi <ginay2521@gmail.com>
2021-05-12 14:20:16 -0400

    initial commit

---
+++ src/test/java/com/tecknews/TechNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecknews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
