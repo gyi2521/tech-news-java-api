b8af42644246af7535ec3906c811fcccccfef3bb
nhyi <ginay2521@gmail.com>
2021-05-21 13:01:01 -0400

    copied everything from tech-news2-java-api with new spring boot version because of session.seconds error

--- src/main/java/com/technews/TechNewJavaApiApplication.java
+++ src/main/java/com/technews/TechNewJavaApiApplication.java
@@ -1,13 +1,13 @@
-package com.tecknews;
+package com.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechNewsJavaApiApplication {
+public class TechNewJavaApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechNewsJavaApiApplication.class, args);
+		SpringApplication.run(TechNewJavaApiApplication.class, args);
 	}
 
 }
